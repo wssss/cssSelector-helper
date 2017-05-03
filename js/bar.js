3f3b0c808d54e8d3f00fb4aeb198f794d747ea69
weis <weishuai@geekfinancer.com>
2017-05-03 15:55:40 +0800

    http get post better

--- js/bar.js
+++ js/bar.js
@@ -3,10 +3,10 @@
 var Z_KEYCODE = 90;
 var TAB_KEYCODE = 8;
 var cur_index = 0;
-
+var host_name = "";
+var jsonAttrs = {};
 
 var nodeList;
-var tabList = document.querySelectorAll('input.btn');
 var submitEl = document.querySelector('button.btn');
 var selectEl = document.querySelector('.form-sel');
 var listEl = document.querySelector('.input-list');
@@ -19,51 +19,33 @@ function clearBoder(){
 }
 
 var jsonattr = {
-    name : "产品名称",
-    alias : "产品简称",
-    min_amount : "最小认购金额(含)(万)",
-    deadline : "期限(月)",
-    deadline_introduction : "期限说明",
-    update_notification : "更新说明",
-    type : "发行通道",
-    field : "投资领域",
-    start_date : "开始募集时间"
+    name : {name:"产品名称",value:"div#product-detail-container span.fl.lagerTitle"},
+    alias : {name:"产品简称",value:"div#product-detail-container a.toptitle.c-f36e27"},
+    min_amount : {name:"最小认购金额(含)(万)",value:""},
+    deadline :{name:"期限月",value:""},
+    deadline_introduction : {name:"期限说明",value:""},
+    update_notification : {name:"更新说明",value:""},
+    type :{name:"发行通道",value:""},
+    field : {name:"投资领域",value:""},
+    start_date : {name:"开始募集时间",value:""}
 }
+var choices = {0:'集合信托', 1:"集合资管",2:"债权基金",3:"证券基金"}
 
-var handleHost = function(request, sender, cb){
-    if(request.host == 'host'){
-        chrome.runtime.sendMessage({
-            method:'GET',
-            action:'xhttp',
-            url:'http://mm.geekfinancer.com/api/entities/companies/?limit=3host=' + request.hostName
-        },function(responseText){
-            innerAttrs();
-            console.log('success');
-        })
+function innerOptions(){
+    var allOptions = '';
+    for(var key in choices){
+        var inOption = '<option value=' + key + '>' + choices[key] + '</option>'
+        allOptions = allOptions + inOption;
     }
+    selectEl.innerHTML = allOptions;
 }
-
-chrome.runtime.onMessage.addListener(handleHost)
-
-// function getAttrs(){
-//     chrome.runtime.sendMessage({
-//         method: 'GET',
-//         action: 'xhttp',
-//         url: 'http://mm.geekfinancer.com/api/entities/companies/?limit=3',
-//         data: 'q=something'
-//     }, function(responseText) {
-//         /*Callback function to deal with the response*/
-//
-//     });
-// }
-// getAttrs()
 
 function innerAttrs(){
     var allInput = ''
     for(var key in jsonattr){
         var inhtml = '<div class="form-group">\
-        <label for="inputEmail3" class="col-sm-2 control-label">' + jsonattr[key] +'</label>\
-        <div class="col-sm-10"><input type="text" class="form-control">\
+        <label for="inputEmail3" class="col-sm-2 control-label">' + jsonattr[key].name +'</label>\
+        <div class="col-sm-10"><input type="text" class="form-control" value=' + jsonattr[key].value + '>\
         </div></div>';
         allInput = allInput + inhtml;
     }
@@ -71,8 +53,41 @@ function innerAttrs(){
     inputInit()
 }
 
+selectEl.addEventListener('change',function(e){
+    getProductAttrs(this.options[this.selectedIndex].text);
+})
+
+//初始化页面数据,接受host，并且请求product_type
+var handleHost = function(request, sender, cb){
+    if(request.host == 'host'){
+        host_name = request.hostName;
+        chrome.runtime.sendMessage({
+            method:'GET',
+            action:'xhttp',
+            url:'http://mm.geekfinancer.com/api/entities/companies/?limit=3host=' + request.hostName
+        },function(responseText){
+            getProductAttrs(responseText[0]);
+        })
+    }
+}
+
+function getProductAttrs(product_type){
+    var url = 'http://mm.geekfinancer.com/api/entities/companies/';
+    //var url = "http://mm.geekfinancer.com/api/parser/selectors/?host=" +　host_name + "&product_type=" + product_type;
+    chrome.runtime.sendMessage({
+        method:'GET',
+        action:'xhttp',
+        url:url
+    }, function(responseText){
+        innerAttrs();
+        innerOptions();
+    })
+}
+
+chrome.runtime.onMessage.addListener(handleHost);
+
 function inputInit(){
-    var nodeList = document.querySelectorAll('input.form-control');
+    nodeList = document.querySelectorAll('input.form-control');
 
     Array.prototype.forEach.call(nodeList,function(el,index){
         el.onfocus = (function(index) {
@@ -83,23 +98,6 @@ function inputInit(){
                 cur_index = index;
             }
         })(index);
-    })
-
-    Array.prototype.forEach.call(tabList, function(el, index){
-        el.onclick = (function(index){
-            return function(){
-                if(el.classList.contains('btn-primary')) return;
-                for(var i = 0; i < tabList.length; i++){
-                    if(i != index){
-                        tabList[i].classList.remove('btn-primary');
-                        tabList[i].classList.add('btn-default');
-                    }else {
-                        el.classList.remove('btn-default');
-                        el.classList.add('btn-primary');
-                    }
-                }
-            }
-        })(index)
     })
 
     var handleRequest = function(request, sender, cb){
@@ -117,15 +115,21 @@ function inputInit(){
 }
 
 submitEl.onclick = function(){
+    var formInput = jsonattr;
+    var i = 0;
+    console.log(nodeList);
+    for(var key in formInput){
+        formInput[key].value = nodeList[i].value
+        i ++;
+    }
+
     chrome.runtime.sendMessage({
-        method: 'GET',
+        method: 'POST',
         action: 'xhttp',
         url: 'http://mm.geekfinancer.com/api/entities/companies/?limit=3',
-        data: 'q=something'
+        data: formInput
     }, function(responseText) {
-        /*Callback function to deal with the response*/
-        innerAttrs();
-        console.log('success');
+        console.log('data send OK!');
     });
 }
 
